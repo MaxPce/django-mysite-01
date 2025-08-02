ce696223bc9e0f8942b57dbf726dab5547cac1f6
MaxPce <max.ponce@tecsup.edu.pe>
2025-08-02 18:10:15 -0500

    1er commit

--- web/views.py
+++ web/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'web/home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'web/about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
-
-
+    return render(request, 'web/contact.html')
